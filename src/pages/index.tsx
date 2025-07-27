e0e878664bf689dbec6a5e0ab788abe77750d750
Azimxan <azimabdulhanov88@gmail.com>
2025-07-27 22:38:26 +0500

    Initial blog project ✨

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+const IndexPage = () => {
+  return (
+    <>
+      <Head>
+        <title>IndexPage</title>
+        <meta name="description" content="IndexPage" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>IndexPage</div>
+    </>
+  )
+}
+
+export default IndexPage
